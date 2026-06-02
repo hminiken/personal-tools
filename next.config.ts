a9295a34d68d632bf1436709daca0f0d8ea958a2
Hillary Miniken <33667037+hminiken@users.noreply.github.com>
2026-06-02 15:09:59 -0700

    update limit to 5 mb

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb', // Increases the limit to 5 megabytes
+    },
+  },
 };
 
 export default nextConfig;
