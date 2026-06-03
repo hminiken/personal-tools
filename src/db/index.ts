ebc73ee98f9fbdd465e57aa6218bdd1ca346e0c4
Hillary Miniken <33667037+hminiken@users.noreply.github.com>
2026-06-03 15:46:54 -0700

    fix db path

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,12 +2,11 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
 
-const dbUrl = process.env.DB_PATH 
-  || (process.env.NODE_ENV === 'production' ? 'file:/app/data/sqlite.db' : 'file:sqlite.db');
+const dbUrl = process.env.DATABASE_URL 
+  || 'file:/app/data/sqlite.db'; // Absolute path fallback!
 
 const client = createClient({ 
   url: dbUrl 
 });
 
 export const db = drizzle(client, { schema });
-
